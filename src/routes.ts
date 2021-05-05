2cf5e1929cf47fe4569a954bb73da51039cb427e
Weri Oliveira <werioliveira@hotmail.com>
2021-05-05 12:47:10 -0300

    add route default

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,9 @@ import {Router} from 'express'
 import {getPlayer} from './class/User'
 
 const route = Router()
+route.use('/',(req,res) =>{
+    return res.json("Digite nome de invocador após o / da url")
+})
 route.use('/:params', getPlayer)
 
 export default route
